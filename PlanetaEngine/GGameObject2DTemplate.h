0e4a4b2cf5c3980a1e723f11abd84501f6083af0
Cdec <cdecrement@gmail.com>
2016-05-20 00:33:28 +0900

    CGameObject2Dテンプレート追加。 Ground2DのGameObjectとGOテンプレート削除。

--- PlanetaEngine/GGameObject2DTemplate.h
+++ PlanetaEngine/GGameObject2DTemplate.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "GameObject.h"
+#include "GIGameObject2D.h"
+
+namespace planeta_engine {
+	template<class... GOI>
+	class GGameObject2DTemplate : public GameObject<GIGameObject2D, GOI...>{
+	public:
+		CTransform2D& transform2d()override { *transform2d_; }
+	protected:
+		bool OnInitialized()override {
+			transform2d_.reset(CreateAndAddComponent<CTransform2D>());
+			return true;
+		}
+	private:
+		utility::NonOwingPointer<CTransform2D> transform2d_;
+	};
+}
--- PlanetaEngine/GIGameObject2D.h
+++ PlanetaEngine/GIGameObject2D.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "GameObjectInterface.h"
+#include "CTransform2D.h"
+
+namespace planeta_engine {
+	class GIGameObject2D : public GameObjectInterface{
+	public:
+		virtual CTransform2D& transform2d() = 0;
+		const CTransform2D& transform2d()const { return const_cast<GIGameObject2D*>(this)->transform2d(); }
+	};
+}
