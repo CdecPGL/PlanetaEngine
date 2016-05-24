4fe55311c3b638af76b5e04f9da0ad32a28e340d
Cdec <cdecrement@gmail.com>
2016-05-25 00:22:06 +0900

    PlanetaEngineクラスの名前空間をplaneta_engine直下に移動。

--- PlanetaEngine/PlanetaEngine.cpp
+++ PlanetaEngine/PlanetaEngine.cpp
@@ -10,101 +10,95 @@
 #include "DrawManager.h"
 
 namespace planeta_engine {
-	namespace core {
-		PlanetaEngine::PlanetaEngine() :is_initialized_(false)
+	using namespace core;
+	PlanetaEngine::PlanetaEngine() :is_initialized_(false) {
+
+	}
+
+	PlanetaEngine::~PlanetaEngine() {
+		assert(!is_initialized_);
+	}
+
+	bool PlanetaEngine::Initialize() {
+		if (is_initialized_) { assert(false); return false; }
+		assert(game_ != nullptr);
+		init_end::CreateSystemDirectory(); //システムディレクトリ作成
+		if (InitializeDebugSystem() //デバッグシステム初期化
+			&& init_end::LoadConfigData() //設定データ読み込み
+			&& InitializeEngine() //エンジン初期化
+			&& game_->Initialize()) //ゲーム初期化
 		{
+			debug::SystemLog::instance().LogMessage("PlanetaEngineを初期化しました。", __FUNCTION__);
+			is_initialized_ = true;
+			return true;
+		} else {
+			debug::SystemLog::instance().LogError("PlanetaEngineの初期化に失敗しました。", __FUNCTION__);
+			is_initialized_ = true;
+			Finalize();
+			return false;
+		}
+	}
 
+	void PlanetaEngine::Finalize() {
+		if (!is_initialized_) { assert(false); return; }
+		assert(game_ != nullptr);
+		game_->Finalize();
+		FinalzieEngine();
+		debug::SystemLog::instance().LogMessage("PlanetaEngineを終了しました。", __FUNCTION__);
+		FinalizeDebugSystem();
+		is_initialized_ = false;
+	}
+
+	PlanetaEngine::Status PlanetaEngine::Update() {
+		if (ProcessMessage() < 0) { return Status::Quit; } //DXライブラリの更新処理
+
+		Status status;
+
+		core::Game::Status gstatue = game_->Update();
+
+		switch (gstatue) {
+		case planeta_engine::core::Game::Status::Continue:status = Status::Continue; break;
+		case planeta_engine::core::Game::Status::Quit:status = Status::Quit; break;
+		case planeta_engine::core::Game::Status::Error:status = Status::Error; break;
+		default:status = Status::Error; break;
 		}
 
-		PlanetaEngine::~PlanetaEngine()
-		{
-			assert(!is_initialized_);
+		debug::SystemCounter::instance().IncrementFrameCount();
+		if (status == Status::Continue) {
+			if (DrawManager::instance().Update() == false) { status = Status::Error; }
 		}
+		return status;
+	}
 
-		bool PlanetaEngine::Initialize()
-		{
-			if (is_initialized_) { assert(false); return false; }
-			assert(game_ != nullptr);
-			init_end::CreateSystemDirectory(); //システムディレクトリ作成
-			if (InitializeDebugSystem() //デバッグシステム初期化
-				&& init_end::LoadConfigData() //設定データ読み込み
-				&& InitializeEngine() //エンジン初期化
-				&& game_->Initialize()) //ゲーム初期化
-			{
-				debug::SystemLog::instance().LogMessage("PlanetaEngineを初期化しました。", __FUNCTION__);
-				is_initialized_ = true;
-				return true;
-			} else {
-				debug::SystemLog::instance().LogError("PlanetaEngineの初期化に失敗しました。", __FUNCTION__);
-				return false;
-			}
+	bool PlanetaEngine::InitializeEngine() {
+		using namespace planeta_engine::core::init_end;
+
+		if (InitDxLibrary() //DXライブラリの初期化
+			&& InstantiateSingletonManagers() //シングルトンマネージャのインスタンス化
+			&& SetUpSingletonManagers() //シングルトンマネージャのセットアップ
+			&& InitializeSingletonManagers() //シングルトンマネージャの初期化
+			) {
+			debug::SystemLog::instance().LogMessage("PlanetaEngine本体を初期化しました。", __FUNCTION__);
+			return true;
+		} else {
+			debug::SystemLog::instance().LogError("PlanetaEngine本体の初期化に失敗しました。", __FUNCTION__);
+			return false;
 		}
+	}
 
-		void PlanetaEngine::Finalize()
-		{
-			if (!is_initialized_) { assert(false); return; }
-			assert(game_ != nullptr);
-			game_->Finalize();
-			FinalzieEngine();
-			debug::SystemLog::instance().LogMessage("PlanetaEngineを終了しました。", __FUNCTION__);
-			FinalizeDebugSystem();
-			is_initialized_ = false;
-		}
+	void PlanetaEngine::FinalzieEngine() {
+		using namespace planeta_engine::core::init_end;
+		FinalizeSingletonManagers(); //シングルトンマネージャの終了処理
+		DisposeSingletonManagers(); //シングルトンマネージャの破棄
+		EndDxLibrary(); //DXライブラリの終了処理
+		debug::SystemLog::instance().LogMessage("PlanetaEngine本体を終了しました。", __FUNCTION__);
+	}
 
-		PlanetaEngine::Status PlanetaEngine::Update()
-		{
-			if (ProcessMessage() < 0) { return Status::Quit; } //DXライブラリの更新処理
-			
-			Status status;
+	bool PlanetaEngine::InitializeDebugSystem() {
+		return init_end::InitializeDebugSystem();
+	}
 
-			Game::Status gstatue = game_->Update();
-
-			switch (gstatue)
-			{
-			case planeta_engine::core::Game::Status::Continue:status = Status::Continue; break;
-			case planeta_engine::core::Game::Status::Quit:status = Status::Quit; break;
-			case planeta_engine::core::Game::Status::Error:status = Status::Error; break;
-			default:status = Status::Error; break;
-			}
-
-			debug::SystemCounter::instance().IncrementFrameCount();
-			if (status == Status::Continue) {
-				if (DrawManager::instance().Update()==false) { status = Status::Error; }
-			}
-			return status;
-		}
-
-		bool PlanetaEngine::InitializeEngine() {
-			using namespace planeta_engine::core::init_end;
-			
-			if (InitDxLibrary() //DXライブラリの初期化
-				&& InstantiateSingletonManagers() //シングルトンマネージャのインスタンス化
-				&& SetUpSingletonManagers() //シングルトンマネージャのセットアップ
-				&& InitializeSingletonManagers() //シングルトンマネージャの初期化
-				) {
-				debug::SystemLog::instance().LogMessage("PlanetaEngine本体を初期化しました。", __FUNCTION__);
-				return true;
-			} else {
-				debug::SystemLog::instance().LogError("PlanetaEngine本体の初期化に失敗しました。", __FUNCTION__);
-				return false;
-			}
-		}
-
-		void PlanetaEngine::FinalzieEngine() {
-			using namespace planeta_engine::core::init_end;
-			FinalizeSingletonManagers(); //シングルトンマネージャの終了処理
-			DisposeSingletonManagers(); //シングルトンマネージャの破棄
-			EndDxLibrary(); //DXライブラリの終了処理
-			debug::SystemLog::instance().LogMessage("PlanetaEngine本体を終了しました。", __FUNCTION__);
-		}
-
-		bool PlanetaEngine::InitializeDebugSystem() {
-			return init_end::InitializeDebugSystem();
-		}
-
-		void PlanetaEngine::FinalizeDebugSystem() {
-			init_end::FinalizeDebugStstem();
-		}
-
+	void PlanetaEngine::FinalizeDebugSystem() {
+		init_end::FinalizeDebugStstem();
 	}
 }
--- PlanetaEngine/PlanetaEngine.h
+++ PlanetaEngine/PlanetaEngine.h
@@ -15,39 +15,39 @@
 namespace planeta_engine {
 	namespace core {
 		class Game;
-		/*ゲームクラスはこのクラスを継承し、初期化を定義する。*/
-		class PlanetaEngine final: public utility::StaticSingletonTemplate<PlanetaEngine>{
-			friend utility::StaticSingletonTemplate<PlanetaEngine>;
-		public:
-			/*ゲームの作成(初期化前に行う)*/
-			template<class GameType>
-			bool CreateGameInstance() {
-				game_ = std::make_unique<GameType>();
-				return game_ != nullptr;
-			}
-			/*エンジンの初期化(失敗した場合でも終了処理を呼び出す必要がある)*/
-			bool Initialize()override;
-			/*エンジンの終了処理*/
-			void Finalize()override;
-			/*エンジンのステータス*/
-			enum class Status { Continue, Quit, Error };
-			/*エンジンの更新*/
-			Status Update();
-		private:
-			PlanetaEngine();
-			~PlanetaEngine();
-			PlanetaEngine(const PlanetaEngine&) = delete;
-			PlanetaEngine(PlanetaEngine&&) = delete;
-			PlanetaEngine& operator=(const PlanetaEngine&) = delete;
-			PlanetaEngine& operator=(PlanetaEngine&&) = delete;
-			bool is_initialized_;
-			std::unique_ptr<Game> game_;
-
-			bool InitializeEngine();
-			void FinalzieEngine();
-
-			bool InitializeDebugSystem();
-			void FinalizeDebugSystem();
-		};
 	}
+	/*ゲームクラスはこのクラスを継承し、初期化を定義する。*/
+	class PlanetaEngine final : public utility::StaticSingletonTemplate<PlanetaEngine> {
+		friend utility::StaticSingletonTemplate<PlanetaEngine>;
+	public:
+		/*ゲームの作成(初期化前に行う)*/
+		template<class GameType>
+		bool CreateGameInstance() {
+			game_ = std::make_unique<GameType>();
+			return game_ != nullptr;
+		}
+		/*エンジンの初期化*/
+		bool Initialize()override;
+		/*エンジンの終了処理*/
+		void Finalize()override;
+		/*エンジンのステータス*/
+		enum class Status { Continue, Quit, Error };
+		/*エンジンの更新(初期化が正常に行われていない状態での呼び出しは未定義動作)*/
+		Status Update();
+	private:
+		PlanetaEngine();
+		~PlanetaEngine();
+		PlanetaEngine(const PlanetaEngine&) = delete;
+		PlanetaEngine(PlanetaEngine&&) = delete;
+		PlanetaEngine& operator=(const PlanetaEngine&) = delete;
+		PlanetaEngine& operator=(PlanetaEngine&&) = delete;
+		bool is_initialized_;
+		std::unique_ptr<core::Game> game_;
+
+		bool InitializeEngine();
+		void FinalzieEngine();
+
+		bool InitializeDebugSystem();
+		void FinalizeDebugSystem();
+	};
 }
