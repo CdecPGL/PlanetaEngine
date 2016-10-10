e8b1114eaa0137dd318b8380a62ecb10913b9dd2
Cdec <cdecrement@gmail.com>
2016-10-10 20:40:00 +0900

    度とラジアンの変換関数追加

--- PlanetaEngine/MathUtility.h
+++ PlanetaEngine/MathUtility.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include "MathConstant.h"
+
 namespace planeta {
 	namespace math {
 		/*値が2のべき乗数か調べる*/
@@ -10,5 +12,15 @@ namespace planeta {
 		bool DoubleIsZero(double d, double epsilon = 1e-5);
 		/*doubleが等しいか絶対誤差を指定して判定*/
 		bool DoubleIsEqual(double d1, double d2, double epsilon = 1e-5);
+		/*度からラジアンに変換*/
+		template<typename T>
+		constexpr T DegToRad(const T& deg) {
+			return static_cast<T>(static_cast<double>(deg) / 180.0 * PI);
+		}
+		/*ラジアンから度に変換*/
+		template<typename T>
+		constexpr T RadToDeg(const T& rad) {
+			return static_cast<T>(static_cast<double>(rad) / PI * 180.0);
+		}
 	}
 }
