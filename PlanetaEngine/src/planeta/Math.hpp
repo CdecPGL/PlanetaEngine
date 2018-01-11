5ce3e82612266ec2abb527cb44a86b4968409ebd
Cdec <cdecrement@gmail.com>
2018-01-11 23:47:42 +0900

    mathをまとめてインクルードするヘッダファイルを追加

---
+++ PlanetaEngine/src/planeta/Math.hpp
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "math/MathConstant.hpp"
+#include "math/MathMatrix.hpp"
+#include "math/MathUtility.hpp"
+#include "math/MathVector.hpp"
+#include "math/MathVectorElementIdentifer.hpp"
